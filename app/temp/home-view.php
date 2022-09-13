a009328898d9f413ef3de4da9d3d7fee8982fb43
Viacheslav <nice.kirat@mail.ru>
2022-09-14 01:51:18 +0600

    start new project

---
+++ app/temp/home-view.php
@@ -0,0 +1,35 @@
+<?php 
+/*template home */
+require_once('app/models/m-home.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<!-- Favicon-->
+	<link rel="shortcut icon" href="img/fav.png">
+	<!-- Author Meta -->
+	<meta name="author" content="CodePixar">
+	<!-- Meta Description -->
+	<meta name="description" content="">
+	<!-- Meta Keyword -->
+	<meta name="keywords" content="">
+	<!-- meta character set -->
+	<meta charset="UTF-8">
+	<!-- Site Title -->
+	<title>Это домашняя страница</title>
+	<!-- css here -->
+	<link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+	<header>
+		<h1>привет - это главная </h1>
+	</header>
+	<p>получаем данные</p>
+	<?php 
+
+	 ?>
+</body>
+</html>
